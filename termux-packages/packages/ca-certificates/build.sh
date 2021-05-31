9025db185852aa49702aa420064243a79f9b5e3e
JunkHack <junkhack@gpl.jp>
2021-05-31 10:45:25 +0900

    Fix --> $ ./build-package.sh php

--- termux-packages/packages/ca-certificates/build.sh
+++ termux-packages/packages/ca-certificates/build.sh
@@ -1,10 +1,11 @@
-TERMUX_PKG_HOMEPAGE=https://curl.haxx.se/docs/caextract.html
+TERMUX_PKG_HOMEPAGE=https://curl.se/docs/caextract.html
 TERMUX_PKG_DESCRIPTION="Common CA certificates"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=20201027
-TERMUX_PKG_SRCURL=https://curl.haxx.se/ca/cacert.pem
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=20210525
+TERMUX_PKG_SRCURL=https://curl.se/ca/cacert.pem
 # If the checksum has changed, it may be time to update the package version:
-TERMUX_PKG_SHA256=bb28d145ed1a4ee67253d8ddb11268069c9dafe3db25a9eee654974c4e43eee5
+TERMUX_PKG_SHA256=3a32ad57e7f5556e36ede625b854057ac51f996d59e0952c207040077cbe48a9
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
